919995869e7740f30b53b911ca0f49326bab6d93
Jon Ander Besga <jonan.bsg@gmail.com>
2017-09-17 12:41:16 +0100

    first commit

---
+++ hackernews.js
@@ -0,0 +1,88 @@
+const request = require('request');
+const TelegramBot = require('node-telegram-bot-api')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+
+const TOKEN = process.env.TOKEN
+const CHANNEL_CHAT_ID = process.env.CHANNEL_CHAT_ID
+const AMOUNT_OF_STORIES = 5
+
+const bot = new TelegramBot(TOKEN)
+
+function sendStoriesMessage(storiesList, message, index=0){
+    if(index >= storiesList.length){
+        console.log(message)
+        bot.sendMessage(CHANNEL_CHAT_ID, message, {
+            parse_mode: 'markdown',
+            disable_web_page_preview: true
+        })
+        return
+    }
+
+    const options = {
+        url: `https://hacker-news.firebaseio.com/v0/item/${storiesList[index]}.json`,
+        json: true
+    };
+
+    request(options, function nextStory(error, response, body){
+        const storyTitle = body.title
+        const storyURL = body.url
+        const storyComments = `https://news.ycombinator.com/item?id=${body.id}`
+
+        if(storyURL){
+            message += `${index+1}. [${storyTitle}](${storyURL}) | [Comments](${storyComments})\n`
+        }
+        else{
+            message += `${index+1}. ${storyTitle} | [Comments](${storyComments})\n`
+        }
+        sendStoriesMessage(storiesList, message, index+1)
+    })
+}
+
+function sendDailySummary(error, response, body){
+    askStories = body.slice(0, AMOUNT_OF_STORIES)
+    
+    bot.sendPhoto(CHANNEL_CHAT_ID, 'hn.png', {
+        caption: 'Loading daily #hackernews'
+    }).then(() => {
+        sendStoriesMessage(topStories, '*TOP STORIES*\n');
+        sendStoriesMessage(showStories, '*SHOW STORIES*\n');
+        sendStoriesMessage(askStories, '*ASK STORIES*\n');
+    })
+}
+
+function getAskNews(error, response, body){
+    showStories = body.slice(0, AMOUNT_OF_STORIES)
+
+    const options = {
+        url: 'https://hacker-news.firebaseio.com/v0/askstories.json',
+        json: true
+    };
+    request(options, sendDailySummary)
+}
+
+function getShowNews(error, response, body){
+    topStories = body.slice(0, AMOUNT_OF_STORIES)
+
+    const options = {
+        url: 'https://hacker-news.firebaseio.com/v0/showstories.json',
+        json: true
+    };
+    request(options, getAskNews)
+}
+
+function getTopNews(){
+    const options = {
+        url: 'https://hacker-news.firebaseio.com/v0/topstories.json',
+        json: true
+    };
+    request(options, getShowNews)
+}
+
+var topStories = []
+var showStories = []
+var askStories = []
+
+getTopNews()
---
+++ producthunt.js
@@ -0,0 +1,63 @@
+const request = require('request');
+const TelegramBot = require('node-telegram-bot-api')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+
+const TOKEN = process.env.TOKEN
+const CHANNEL_CHAT_ID = process.env.CHANNEL_CHAT_ID
+
+const bot = new TelegramBot(TOKEN)
+
+function sendDailySummary(error, response, body){
+    if (!error && response.statusCode == 200) {
+        
+        let message = '*TOP DAILY PRODUCTS*\n\n'
+
+        body.posts.forEach(function(post, index) {
+            message += `${index+1}. [${post.name}](${post.redirect_url}) ([${post.votes_count}](${post.discussion_url}))\n_${post.tagline}_\n`
+        });
+
+        bot.sendPhoto(CHANNEL_CHAT_ID, 'ph.png', { 
+            caption: 'Loading daily #producthunt'
+        }).then(() => {
+            bot.sendMessage(CHANNEL_CHAT_ID, message, {
+                parse_mode: 'markdown',
+                disable_web_page_preview: true
+            })
+        })
+    }
+}
+
+function getTodayPosts(error, response, body){
+    if (!error && response.statusCode == 200) {
+
+        const options = {
+            url: 'https://api.producthunt.com/v1/posts',
+            headers: {    
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Host': 'api.producthunt.com',
+                'Authorization': `Bearer ${body.access_token}`
+            },
+            json: true
+        };
+        request(options, sendDailySummary)
+    }
+}
+
+const options = {
+    url: 'https://api.producthunt.com/v1/oauth/token',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Host': 'api.producthunt.com'
+    },
+    json: {
+        client_id: '7a80da59a58ce2c6d8a3c657d2784192ffaba695bf801560289f44e191bb4cc2',
+        client_secret: '2f151d31ac00292168366c44cd11ae47d2c8066ffe583b7472a6dffc565616e0',
+        grant_type: 'client_credentials'
+    }
+};
+request.post(options, getTodayPosts)
